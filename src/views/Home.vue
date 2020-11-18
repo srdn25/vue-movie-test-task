ae70ca977a442c151d68b9a559187e7d180c99fd
srdn <srdn2417@gmail.com>
2020-11-18 12:20:52 +0200

    add user name instead of TODO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Batman logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello #TODO#, which batman movie do you want to see?"/>
+    <HelloWorld v-bind:msg="'Hello ' + user.name +', which batman movie do you want to see?'"/>
     <MovieList />
   </div>
 </template>
@@ -10,12 +10,16 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import MovieList from '@/components/MovieList.vue';
+import { mapState } from 'vuex';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
     MovieList,
+  },
+  computed: {
+    ...mapState(['user']),
   },
 };
 </script>
